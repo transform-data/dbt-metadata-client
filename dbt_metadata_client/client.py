ffbaacb8c92718519c243a99ae75ff6fad5efa7c
Thomas Quigley Malcolm <thomas@transformdata.io>
2022-10-31 10:34:58 -0700

    Add query helpers to `Client` for base metadata objects
    Specifically adds query support for
    - Exposures
    - Macros
    - Metrics
    - Models
    - Snapshots
    - Seeds
    - Sources
    - Tests

--- dbt_metadata_client/client.py
+++ dbt_metadata_client/client.py
@@ -1,7 +1,19 @@
-from typing import Any, Dict, Union
+from typing import Any, Dict, List, Union
 
 from sgqlc.endpoint.http import HTTPEndpoint
 from sgqlc.operation import Operation
+
+from dbt_metadata_client.dbt_metadata_api_schema import (
+    ExposureNode,
+    MacroNode,
+    MetricNode,
+    ModelNode,
+    SeedNode,
+    SnapshotNode,
+    SourceNode,
+    TestNode,
+    dbt_metadata_api_schema,
+)
 
 
 class Client:
@@ -16,3 +28,67 @@ class Client:
     def query_operation(self, operation: Operation) -> Union[Any, Dict[str, Any]]:  # type: ignore[misc]
         """Simple query helper which takes in an SGQLC Operation"""
         return self.endpoint(query=operation)
+
+    def get_exposures(self, job_id: int) -> List[ExposureNode]:
+        """Get the Exposures associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.exposures(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        exposures: List[ExposureNode] = (query_op + data).exposures
+        return exposures
+
+    def get_macros(self, job_id: int) -> List[MacroNode]:
+        """Get the Sources associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.macros(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        macros: List[MacroNode] = (query_op + data).macros
+        return macros
+
+    def get_metrics(self, job_id: int) -> List[MetricNode]:
+        """Get the Metrics associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.metrics(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        metrics: List[MetricNode] = (query_op + data).metrics
+        return metrics
+
+    def get_models(self, job_id: int) -> List[ModelNode]:
+        """Get the Models associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.models(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        models: List[ModelNode] = (query_op + data).models
+        return models
+
+    def get_snapshots(self, job_id: int) -> List[SnapshotNode]:
+        """Get the Snapshots associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.snapshots(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        snapshots: List[SnapshotNode] = (query_op + data).snapshots
+        return snapshots
+
+    def get_seeds(self, job_id: int) -> List[SeedNode]:
+        """Get the Seeds associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.seeds(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        seeds: List[SeedNode] = (query_op + data).seeds
+        return seeds
+
+    def get_sources(self, job_id: int) -> List[SourceNode]:
+        """Get the Sources associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.sources(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        sources: List[SourceNode] = (query_op + data).sources
+        return sources
+
+    def get_tests(self, job_id: int) -> List[TestNode]:
+        """Get the Tests associated with a dbt job"""
+        query_op = Operation(dbt_metadata_api_schema.Query)
+        query_op.tests(job_id=job_id)
+        data = self.query_operation(operation=query_op)
+        tests: List[TestNode] = (query_op + data).tests
+        return tests
